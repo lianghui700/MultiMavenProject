cca2146a5bfb07365a930bd3703ab7d31ce42add
lianghui <741712074@qq.com>
2018-02-28 11:33:26 +0800

    add multimavenproject

---
+++ multi-maven-provider/src/main/java/com/huntkey/ruixin/multiModules/App.java
@@ -0,0 +1,13 @@
+package com.huntkey.ruixin.multiModules;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
